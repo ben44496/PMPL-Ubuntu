752ae9c47915da6a3c67165ca86c558948811e92
Benjamin Nguyen <ben44496@gmail.com>
2021-07-06 17:07:02 -0500

    Run script contains all commands needed for setting up on Ubuntu

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ sudo apt update
 # sudo apt upgrade
 
 #### Install packages
-sudo apt install build-essential libboost-all-dev make cmake libmpfr-dev libeigen3-dev
+sudo apt install -y build-essential libboost-all-dev make cmake libmpfr-dev libeigen3-dev
 # sudo apt upgrade
 
 #### Install Qt4
-sudo add-apt-repository ppa:rock-core/qt4
+sudo add-apt-repository -y ppa:rock-core/qt4
 sudo apt-get update
-sudo apt-get install qt4-default
+sudo apt-get install -y qt4-default
 
 #### Inside PMPL/
 git clone git@gitlab.engr.illinois.edu:parasol-group/parasol/pmpl.git
@@ -33,14 +33,19 @@ cd vizmo/
 git checkout ubuntu-20-04
 cd ..
 cd vizmo
-git clone git@gitlab.engr.illinois.edu:parasol-group/parasol/pmpl.git --depth 1 --branch vizmo
+git clone git@gitlab.engr.illinois.edu:parasol-group/parasol/pmpl.git --depth 1 --branch ubuntu-20-04-vizmo
 cd pmpl
 ln -s ../../pmpl_utils
 cd ../src
 make -j8 platform=Ubuntu
+
 #### ubuntu problems here
 #### Makefile.VizmoDefaults:34: *** Qt moc not found at /usr/lib64/qt4/bin/moc..  Stop.
 #### ubuntu problems end
 
+#### Making PMPL
+cd ../../pmpl/src
+make -j8 platform=Ubuntu
+make -j8 platform=Ubuntu pmpl
 #### Compiling and running the code section 3 PMPL checkout 
 #### Add export PATH=~/PMPL/vizmo/src/:$PATH to bashrc?
